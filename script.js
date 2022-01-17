20709cb21f531fa37e7907d05927d0c81494f57b
Ha Chi HOANG <chibienayme@gmail.com>
2022-01-17 13:26:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+// console.log("Hello World");
+// console.log("Bonjour");
+
+// const username = "Marie";
+
+// console.log(username);
+
+// function $()
+// console.log($);
+
+// $("#text")
+// console.log("#text");
+
+// Callback dans une fonction
+//V1
+$(() => {
+  // la methode hide: $("#text").hide()
+  // $("button").click(function () {
+  // console.log(" You clicked the button");
+  // });
+  // $("button").click(function () {
+  //     $("#text").hide();
+  // })
+
+  //Version 1
+  // $("button").click(function () {
+  //     const text = $("#text");
+
+  //     if ($("#text").is(":visible")) {
+  //         $("#text").hide();
+  //     }else {
+  //         $("#text").show();
+  //     };
+  // })
+
+  //Version 2
+  // $("button").click(function () {
+  //     const text = $("#text");
+
+  //     if ($("#text").is(":visible")) {
+  //         text.hide();
+  //     }else {
+  //         text.show();
+  //     };
+  // })
+
+  //Version 3
+  $("clickMe").click(function () {
+    const text = $("#text");
+
+    if ($("#text").is(":visible")) {
+      text.hide();
+    } else {
+      text.show();
+    }
+
+    // $(".h1").addClass("arial"); //change font 
+    // $(".h1").text("My new title"); //change text
+    $(".h1").addClass("arial").text()
+    //Recupere toutes les balises html qui ont la class "title"
+    const titles = $(".title");
+    titles.hover(function () {
+      // titles[0].style.color = "red";
+    //   console.log(title);
+
+    titles.css("color", "red");
+
+    });
+  });
+
+  // Exemples
+  $("#loadCountries").click(function () {
+    // "https://restcountries.com/v3.1/all"
+    $.ajax({
+      url:"https://restcountries.com/v3.1/all";
+      success: function (data) {
+        // console.log(data);
+        // data est un tableau d'objet
+        // chaque objet est un pays
+        data.forEach((country) => {
+          // inserier un "p" a la fin du div
+          // possedant l'id "countries-list"
+          $("#countries-list").append(`<p> ${country.name.common} </p>`)
+        })
+      }
+    });
+  });
+});
+    
+ 
+
+//V2
+// $(function () {});
