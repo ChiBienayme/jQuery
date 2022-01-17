85b8c86e37ea4a2ab3f5541b2154305b68d61adf
Ha Chi HOANG <chibienayme@gmail.com>
2022-01-17 15:43:58 +0100

    3rd commit

---
+++ ex/digitous-jquery-main/exercice8/js/main.js
@@ -0,0 +1,14 @@
+// Le texte Konexio! est dans le code HTML mais invisible à l'oeil de l'utilisateur. Faites le apparaître après un clic sur le bouton magique. Ce bouton appelera la fonction displaySecretText. Cette fonction a déjà été intégrée à la page et ne fait pas partie de jQuery. Il vous suffira de l'appeler
+
+
+
+// $(".btn").click(function(){
+//     $("#text").css("display", "contents");
+//   });
+
+$("button").click(function displaySecretText(){
+  $("#text").show(function() {
+  });
+});
+  
+  
---
+++ ex/digitous-jquery-main/exercice9/js/main.js
@@ -0,0 +1,7 @@
+// Le texte Konexio! est dans le code HTML mais invisible à l'oeil de l'utilisateur. Faites le apparaître après un clic sur le bouton magique. Ce bouton appelera une fonction que vous aurez créé et qui affichera le texte avec la méthode css
+
+$(".btn").click(function(){
+    $("#text").css("display", "contents");
+  });
+
+  
