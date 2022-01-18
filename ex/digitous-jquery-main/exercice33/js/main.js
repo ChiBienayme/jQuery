87ec7105aeb5da4ac4aa7de0f65d89d7725c5f1a
Ha Chi HOANG <chibienayme@gmail.com>
2022-01-18 17:18:51 +0100

    6th commit

--- ex/digitous-jquery-main/exercice33/js/main.js
+++ ex/digitous-jquery-main/exercice33/js/main.js
@@ -1,16 +1,42 @@
 
 
 
-$("button").click(function () {
-    $.ajax({
-      url:"https://restcountries.com/v3.1/name/france",
-      success: function (data) {
+// $("button").click(function () {
+//     $.ajax({
+//       url:"https://restcountries.com/v3.1/name/france",
+//       success: function (data) {
         
-        data.forEach((country) => {
+//         data.forEach((country) => {
           
-          $("#country").html(`${country.name.common}`);
-          $("#capital").html(`${country.capital}`);
-        })
-      }
-    });
-  });
+//           $("#country").html(`${country.name.common}`);
+//           $("#capital").html(`${country.capital}`);
+//         })
+//       }
+//     });
+//   });
+
+  //Version 2 avec fetch et await
+
+$("button").click(function() {
+	// $("button").empty();
+	getCountries()
+	.then(result =>{
+		result.forEach(country => {
+			
+			$("#country").html(`${country.name.common}`);
+            $("#capital").html(`${country.capital}`);
+
+		});	
+	})
+	.catch(err =>console.log(err));
+});
+
+
+async function getCountries(){
+
+	const response = await fetch(`https://restcountries.com/v3.1/name/france`);
+
+	const responseData = await response.json();
+
+	return responseData;
+}
--- ex/digitous-jquery-main/exercice34/js/main.js
+++ ex/digitous-jquery-main/exercice34/js/main.js
@@ -1,25 +1,43 @@
+//version 1 avec ajax
+// $("button").click(function () {
+//     $("input").val("");
+//     $.ajax({
+//         url:"https://restcountries.com/v3.1/name/usa",
+//         success: function (data) {
 
-$("button").click(function () {
-    $("input").val("");
-    $.ajax({
-        url:"https://restcountries.com/v3.1/name/usa",
-        success: function (data) {
-
-            data.forEach((country) => {
+//             data.forEach((country) => {
             
-            $("#country").html(`${country.name.common}`);
-            $("#capital").html(`${country.capital}`);
-            })
-        }
-        })
+//             $("#country").html(`${country.name.common}`);
+//             $("#capital").html(`${country.capital}`);
+//             })
+//         }
+//         })
     
-  });
+//   });
 
-// document.addEventListener('button', () => {
-//     let countries;
-//     fetch('https://restcountries.eu/v3.1/name/france')
-//     .then(res => {
+//Version 2 avec fetch et await
 
-//         countries = res.data.countries;
-//     })
-// })
+$("button").click(function() {
+	// $("button").empty();
+	getCountries()
+	.then(result =>{
+		result.forEach(country => {
+			
+			$("#country").html(`${country.name.common}`);
+            $("#capital").html(`${country.capital}`);
+
+		});	
+	})
+	.catch(err =>console.log(err));
+});
+
+
+async function getCountries(){
+
+	const response = await fetch(`https://restcountries.com/v3.1/name/france`);
+
+	const responseData = await response.json();
+
+	return responseData;
+}
+
